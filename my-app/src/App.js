be0a2f0653f9244f2c83b859368bce3952c5b1c1
Svetla17 <SvetBar@gmail.com>
2021-12-20 22:12:52 +0300

    dz5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import './App.css';
 import './ComponentsRoute/Main.css';
+import './Layout/Layout/Layout.css';
 import { Link, NavLink } from "react-router-dom";
 
 
@@ -16,6 +17,10 @@ import Reviews from './ComponentsRoute/Reviews'
 import Menu from './ComponentsRoute/Menu';
 import { Page404 } from './ComponentsRoute/Page404';
 // import ReviewsItem from './ComponentsRoute/ReviewsItem'
+import Header from './Layout/Layout/Header';
+import Content from './Layout/Layout/Content';
+import Sitebar from './Layout/Layout/Sidebar';
+import Footer from './Layout/Layout/Footer';
 
 function ReviewsItem() {
   return <h3>{}</h3>;
@@ -24,20 +29,31 @@ function ReviewsItem() {
 
 function App () {
   return (
-    <div className="App"> 
-      <BrowserRouter> 
-          <Menu></Menu>
-        <Routes> 
-          <Route path="/" element={<Home />}/>            
-          <Route path="contacts" element={<Contacts />}/>              
-          <Route path="reviews" element={<Reviews />} > 
-            <Route path="reviewsItem" element={<ReviewsItem />} />            
-          </Route>
-          <Route path="*" element={<Page404 />} />           
-        </Routes>         
-      </BrowserRouter>
-      
-    </div>
+    <>
+  {/* // <div className="App"> 
+      //   <BrowserRouter> 
+      //       <Menu></Menu>
+      //     <Routes> 
+      //       <Route path="/" element={<Home />}/>            
+      //       <Route path="contacts" element={<Contacts />}/>              
+      //       <Route path="reviews" element={<Reviews />} > 
+      //         <Route path="reviewsItem" element={<ReviewsItem />} />            
+      //       </Route>
+      //       <Route path="*" element={<Page404 />} />           
+      //     </Routes>         
+  //        </BrowserRouter>
+    
+  //      </div> */}
+              
+          <div className="wrapper">
+            <Header/>
+            <Content/>
+            <Sitebar/>
+            <Footer/>
+          </div>
+
+    </>
+
   );
   }
 
---
+++ my-app/src/Layout/Layout/Content.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+
+function Content (){
+
+  return (
+      
+      <div className = "cont">              
+        <h1>Content</h1>
+      </div> )
+
+}
+
+export default Content
+
+
